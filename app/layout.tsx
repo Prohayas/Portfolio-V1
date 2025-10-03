c066a21b0f0d8eeacde7fce12530d208d5687ff5
Melbienri Gabitan <161171332+Prohayas@users.noreply.github.com>
2025-10-03 14:38:10 +0800

    Change script loading strategy for Zoho SalesIQ
    Updated script loading strategy for Zoho SalesIQ widget.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,14 +15,17 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        <Script
-          src="https://cdn.pagesense.io/js/900810103/ace1cf6333294aa1b473b15278782288.js"
-        />
-        <Script>{`
+        <Script id="zoho-init" strategy="beforeInteractive">
           window.$zoho = window.$zoho || {};
           $zoho.salesiq = $zoho.salesiq || { ready: function() {} };
-        `}</Script>
-        <Script src="https://salesiq.zohopublic.com/widget?wc=siq3cc8b17408ef3c43d8f52dac1f0f5c99" defer></Script>
+        `}
+      </Script>
+
+      {/* Load the Zoho SalesIQ widget */}
+      <Script
+        src="https://salesiq.zohopublic.com/widget?wc=siq3cc8b17408ef3c43d8f52dac1f0f5c99"
+        strategy="afterInteractive"
+      />
       </head>
       <body className={`${inter.variable} antialiased`}>
         <main>{children}</main>
